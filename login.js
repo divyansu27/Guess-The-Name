89b1b373fd5772dea3a026bc5e4b49993f1e9d2b
divyansu27 <98471039+divyansu27@users.noreply.github.com>
2022-06-22 19:49:42 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,7 @@
+function login() {
+
+    player_name = document.getElementById("player_name").value;
+    localStorage.setItem("player_name",player_name);
+
+    window.location = "gamepage.html";
+}
